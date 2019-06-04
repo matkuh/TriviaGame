13f0f536228b21923fe1a75fcef7d3edeac10909
matkuh <matkuh@gmail.com>
2019-06-03 18:57:59 -0700

    add actual questions, start timeout function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,51 +1,51 @@
 var timeRemaining = 30000;
 var correct = 0;
+var incorrect = 0;
 var clockRunning = false;
-var question1 = "";
 
 var q1 = {
-    Q: "What is your favorite board game?",
-    a1: "Chess",
-    a2: "Checkers",
-    a3: "Monopoly",
-    a4: "Risk",
-    correct: "Risk"
+    Q: "What is the only U.S. state that only borders one other?",
+    a1: "Rhode Island",
+    a2: "Maine",
+    a3: "Washington",
+    a4: "Florida",
+    correct: "Maine"
 }
 
 var q2 = {
-    Q: "What is your favorite movie",
-    a1: "Batman",
-    a2: "Superman",
-    a3: "Pocahantos",
-    a4: "Red Riding Hood",
-    correct: "Batman"
+    Q: "Name the number that is three more than one-fifth of one-tenth of one-half of 5,000.",
+    a1: "503",
+    a2: "103",
+    a3: "53",
+    a4: "108",
+    correct: "53"
 }
 
 var q3 = {
-    Q: "Who is the coolest",
-    a1: "Jim",
-    a2: "Jerry",
-    a3: "Billy",
-    a4: "Bob",
-    correct: "Jerry"
+    Q: "What is the missing number in this sequence (1, 1, 2, 3, 5, 8, 13, __, 34)?",
+    a1: "20",
+    a2: "21",
+    a3: "25",
+    a4: "17",
+    correct: "21"
 }
 
 var q4 = {
-    Q: "Who is the coolest",
-    a1: "Jim",
-    a2: "Jerry",
-    a3: "Billy",
-    a4: "Bob",
-    correct: "Jerry"
+    Q: "What is the longest time a human spent in space?",
+    a1: "505 days",
+    a2: "1054 days",
+    a3: "235 days",
+    a4: "437 days",
+    correct: "437 days"
 }
 
 var q5 = {
-    Q: "Who is the coolest",
-    a1: "Jim",
-    a2: "Jerry",
-    a3: "Billy",
-    a4: "Bob",
-    correct: "Jerry"
+    Q: "What is the fastest production car in the world?",
+    a1: "Bugatti Chiron",
+    a2: "Lamborghinni Aventador",
+    a3: "McLaren Senna",
+    a4: "Ford GT",
+    correct: "Bugatti Chiron"
 }
 
 var q6 = {
@@ -57,26 +57,32 @@ var q6 = {
     correct: "Jerry"
 }
 
+
+
 var currentQuestion = 0;
 var questions = [q1, q2, q3, q4, q5, q6];
 
+setTimeout(function(rightAnswer){
+    $("#questionBox").html()
+}
+
 function displayResults(){
     if (currentQuestion === 6){
-        $("#questionBox").html("<p>" + "Correct" + correct + "/6" + "</p>")
+        $("#questionBox").html("<p>" + "Correct Answers " + correct + "</p>" + "<p>" + "Incorrect Answers " + incorrect + "</p>")
+        return 
     }
 }
 
 function resetGame(){
-    correct = 0;
     $("#questionBox").html("<button id='restart'>" + "Restart" + "</button>")
-    $("#start").on("click", function() {
+    $("#restart").on("click", function() {
         $("#questionBox").html("<button id='start'>" + "Start" + "</button>")
+        correct = 0;
 })
 }
 
 
 $(document).ready(function(){
-
 $("#questionBox").html("<button id='start'>" + "Start" + "</button>")
 
 $("#start").on("click", function() {
@@ -89,7 +95,7 @@ $("#start").on("click", function() {
 $(document).on("click", ".answer-button", function(event) {
     event.preventDefault();
     var button = $(this).text();
-    if ((button === questions[currentQuestion].correct) && (currentQuestion < 3)){
+    if ((button === questions[currentQuestion].correct) && (currentQuestion < 7)){
         alert("correct!")
         correct++
         currentQuestion++;
@@ -98,6 +104,7 @@ $(document).on("click", ".answer-button", function(event) {
 
     } else {
         alert("incorrect!")
+        incorrect++
         currentQuestion++
         $("#questionBox").html("<p>" + questions[currentQuestion].Q + "</p>" + " <button class=' answer-button'>" + questions[currentQuestion].a1 + "</button>" + "<button class='answer-button'>" + questions[currentQuestion].a2 + "</button>" + "<button class = 'answer-button'>" + questions[currentQuestion].a3 + "</button>" + "<button class = 'answer-button'>" + questions[currentQuestion].a4 + "</button>")
     } 
